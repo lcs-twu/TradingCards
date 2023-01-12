a1536da96f7989e142f93f1bad72982af4a8f701
Tom <twu@lcs.on.ca>
2023-01-12 14:17:20 -0500

    Initial Commit

---
+++ TradingCards/TradingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardsApp.swift
+//  TradingCards
+//
+//  Created by Tom Wu on 2023-01-12.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
