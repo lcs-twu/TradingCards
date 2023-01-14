85c56cea9b6608b5349f3dfc4e88e44e14a76af1
Tom <twu@lcs.on.ca>
2023-01-14 10:09:36 -0500

    frame+energy icon

--- TradingCards/Layout1.swift
+++ TradingCards/Layout1.swift
@@ -12,8 +12,6 @@ struct Layout1: View {
         ZStack{
             LinearGradient(colors: [.yellow, .red], startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
-
-                
             
             VStack{
                 HStack{
@@ -25,11 +23,24 @@ struct Layout1: View {
                     Text("Charizard")
                         .padding()
                         .font(.title)
+                        .fontWeight(.heavy)
+                        
+                    Image("FireIcon")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100.0, height: 100.0)
                     
                 }
-                Image("Charizard")
-                .resizable()
-                .scaledToFit()
+                ZStack{
+                    Color.black
+                        .frame(width: 300.0, height: 300.0)
+                    Color.white
+                        .frame(width: 295.0, height: 295.0)
+                    Image("Charizard")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300.0, height: 300.0)
+                }
                 Text("No.006")
                     .padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 20))
                     .background(Color.yellow)
