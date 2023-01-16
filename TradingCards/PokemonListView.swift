80592c221da34c2fbd9af25e1ef8a1ddb6e333ed
Tom <twu@lcs.on.ca>
2023-01-16 10:00:40 -0500

    navigation link

---
+++ TradingCards/PokemonListView.swift
@@ -0,0 +1,39 @@
+//
+//  PokemonListView.swift
+//  TradingCards
+//
+//  Created by Tom Wu on 2023-01-16.
+//
+
+import SwiftUI
+
+struct PokemonListView: View {
+    var body: some View {
+        List {
+            
+            NavigationLink(destination: {
+                Layout1View(pokemon: Charizard)
+            }, label: {
+                Text(Charizard.pokemonname)
+            })
+            
+            NavigationLink(destination: {
+                Layout1View(pokemon: SlowBro)
+            }, label: {
+                Text(SlowBro.pokemonname)
+            })
+            
+            
+            
+        }
+        .navigationTitle("My Favourite Pokemons")
+    }
+}
+
+struct PokemonListView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView{
+            PokemonListView()
+        }
+    }
+}
